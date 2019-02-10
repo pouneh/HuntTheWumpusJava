acbbceb59bd4252c39fc0de9f2e74a854d9fc856
Pouneh Aghababazadeh <poaghaba@microsoft.com>
2019-02-09 16:36:42 -0800

    Add score calculation to player object

--- src/Player.java
+++ src/Player.java
@@ -3,31 +3,46 @@ import java.util.Optional;
 public class Player extends Entity{
     private int coinCount = 0;
     private int arrowCount = 0;
+    private int turnCount = 0;
+    private boolean wonGame = false;
+
     Player(){
         super();
     }
 
-    private int FoundCoin(Optional<Integer> countFound){
+    public void  EndTurn(){
+        turnCount ++;
+    }
+
+    public void WinGame(){
+        EndTurn();
+        wonGame = true;
+    }
+    public int FoundCoin(Optional<Integer> countFound){
         Integer count = countFound.isPresent() ? countFound.get() : 1;
         coinCount += count;
         return coinCount;
     }
 
-    private int SpendsCoin(Optional<Integer> countSpent){
+    public int SpendsCoin(Optional<Integer> countSpent){
         Integer count = countSpent.isPresent() ? countSpent.get() : 1;
         coinCount -= count;
         return coinCount;
     }
 
-    private int ObtainedArrow(Optional<Integer> countEarned){
+    public int ObtainedArrow(Optional<Integer> countEarned){
         Integer count = countEarned.isPresent() ? countEarned.get() : 1;
         arrowCount += count;
         return arrowCount;
     }
 
-    private int ShootsArrow(Optional<Integer> countShot){
+    public int ShootsArrow(Optional<Integer> countShot){
         Integer count = countShot.isPresent() ? countShot.get() : 1;
         arrowCount -= count;
         return arrowCount;
+    }
+
+    public int GetCurrentScore(){
+        return (100 - turnCount + coinCount + 5*arrowCount + (wonGame ? 50 : 0));
     }
 }
