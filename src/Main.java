d5228582a76c6b226305ca0f42297869715ef656
Pouneh Aghababazadeh <poaghaba@microsoft.com>
2019-02-11 09:24:20 -0800

    Re-adding project files

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//This class will function as the GameController of the project.
+import java.nio.file.Paths;
+
+public class Main {
+    public static int SplashScreenDelay = 2;
+    public static final String WorkingDirectory = Paths.get(System.getProperty("user.dir")).toString();
+    public static final String ResourceDirectory = Paths.get(WorkingDirectory, "resources").toString();
+
+    public static void main(String[] args) {
+        HighScore.Init();
+        GameEntities.Init();
+        //UI.Create();
+
+
+        //UI.instance.ShowSplashScreen();
+        //UI.instance.ShowMainMenu();
+
+
+        HighScore.SaveScores();
+        System.exit(0);
+    }
+}
