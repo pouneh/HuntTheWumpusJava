f1f5abf15d25f7a0d99591ced7109d7caaad2ab5
Pouneh Aghababazadeh <poaghaba@microsoft.com>
2019-02-11 09:23:13 -0800

    gitignore shenanigans

--- src/Entity.java
+++
@@ -1,9 +0,0 @@
-public class Entity{
-    private int roomNumber;
-    public void Move(){
-        roomNumber++; //TODO, move correctly
-    }
-    public int GetRoom(){
-        return roomNumber;
-    }
-}
